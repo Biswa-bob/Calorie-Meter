2d6eae19d6b8dbf880f8e125f1405cb1e6763610
Biswa-bob <biswajeetpanda.bp@gmail.com>
2024-06-26 07:29:30 +0530

    Initial Commit

---
+++ Calorie Meter/Calorie_MeterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calorie_MeterApp.swift
+//  Calorie Meter
+//
+//  Created by Biswajeet Panda on 26/06/24.
+//
+
+import SwiftUI
+
+@main
+struct Calorie_MeterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
